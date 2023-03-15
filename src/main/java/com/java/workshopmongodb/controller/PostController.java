cf914bfab4ab344c1bfa492b4858a513c33e471d
Felipe Campos <felipecamposlv@gmail.com>
2023-03-15 12:23:01 -0300

    RBuscar Post por id

---
+++ src/main/java/com/java/workshopmongodb/controller/PostController.java
@@ -0,0 +1,53 @@
+package com.java.workshopmongodb.controller;
+
+import com.java.workshopmongodb.model.Post;
+import com.java.workshopmongodb.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/posts")
+@RequiredArgsConstructor
+public class PostController {
+
+    private final PostService postService;
+
+    @GetMapping
+    public ResponseEntity<List<Post>> findAll() {
+        List<Post> list = postService.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Post> findById(@PathVariable String id) {
+        Post post = postService.findById(id);
+        return ResponseEntity.ok().body(post);
+    }
+
+    @PostMapping
+    public ResponseEntity<Post> insert(@RequestBody Post post) {
+        Post currentPost = postService.insert(post);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(post.getId()).toUri();
+        return ResponseEntity.created(uri).body(currentPost);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable String id) {
+        postService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Post> update(@PathVariable String id, @RequestBody Post post) {
+        Post currentPost = postService.findById(post.getId());
+        currentPost.setId(id);
+        currentPost = postService.update(post);
+        return ResponseEntity.ok().body(currentPost);
+    }
+
+}
---
+++ src/main/java/com/java/workshopmongodb/service/PostService.java
@@ -0,0 +1,48 @@
+package com.java.workshopmongodb.service;
+
+import com.java.workshopmongodb.model.Post;
+import com.java.workshopmongodb.repository.PostRepository;
+import com.java.workshopmongodb.service.exception.ObjectNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class PostService {
+
+    private final PostRepository postRepository;
+
+    public List<Post> findAll() {
+        return postRepository.findAll();
+    }
+
+    public Post findById(String id) {
+        return postRepository.findById(id).orElseThrow(() ->
+                new ObjectNotFoundException("Object Not Found! ID: " + id + " Type:" + Post.class.getSimpleName()));
+    }
+
+    public Post insert(Post post) {
+        return postRepository.save(post);
+    }
+
+    public void delete(String id) {
+        this.findById(id);
+        postRepository.deleteById(id);
+    }
+
+    public Post update(Post post) {
+        Post currentPost = this.findById(post.getId());
+        updateData(currentPost, post);
+        return postRepository.save(currentPost);
+    }
+
+    private void updateData(Post currentPost, Post post) {
+        currentPost.setDate(post.getDate());
+        currentPost.setBody(post.getBody());
+        currentPost.setTitle(post.getTitle());
+        currentPost.setAuthor(post.getAuthor());
+    }
+
+}
