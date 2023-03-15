4781a6fe0b48e7d3eeed626389eab010c2526ba1
Felipe Campos <felipecamposlv@gmail.com>
2023-03-15 08:23:21 -0300

    OInserção de usuário

--- src/main/java/com/java/workshopmongodb/controller/UserController.java
+++ src/main/java/com/java/workshopmongodb/controller/UserController.java
@@ -5,11 +5,10 @@ import com.java.workshopmongodb.model.User;
 import com.java.workshopmongodb.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -31,5 +30,13 @@ public class UserController {
     public ResponseEntity<UserDTO> findById(@PathVariable String id) {
         User user = userService.findById(id);
         return ResponseEntity.ok().body(new UserDTO(user));
+    }
+
+    @PostMapping
+    public ResponseEntity<UserDTO> insert(@RequestBody UserDTO userDTO) {
+        User user = userService.fromDTO(userDTO);
+        user = userService.insert(user);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+        return ResponseEntity.created(uri).body(new UserDTO(user));
     }
 }
--- src/main/java/com/java/workshopmongodb/service/UserService.java
+++ src/main/java/com/java/workshopmongodb/service/UserService.java
@@ -1,5 +1,6 @@
 package com.java.workshopmongodb.service;
 
+import com.java.workshopmongodb.dto.UserDTO;
 import com.java.workshopmongodb.model.User;
 import com.java.workshopmongodb.repository.UserRepository;
 import com.java.workshopmongodb.service.exception.ObjectNotFoundException;
@@ -21,5 +22,13 @@ public class UserService {
     public User findById(String id) {
         return userRepository.findById(id).orElseThrow(() ->
                 new ObjectNotFoundException("Object Not Found! ID: " + id + " Type:" + User.class.getSimpleName()));
+    }
+
+    public User insert(User user) {
+        return userRepository.save(user);
+    }
+
+    public User fromDTO(UserDTO userDTO) {
+        return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
     }
 }
